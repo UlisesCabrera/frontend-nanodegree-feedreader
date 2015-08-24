9a7bf5b838302ecfc1fc561f044bf6e9f97ceb70
Ulises Cabrera <ucabrera@outlook.com>
2015-08-24 16:56:44 -0400

    added comments to the feedreader.js file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,7 +1,6 @@
 /* global allFeeds */
-
+/* strict */
 $(function () {
-
     describe('RSS Feeds', function () {
         it('are defined', function () {
             expect(allFeeds).toBeDefined();
@@ -35,41 +34,63 @@ $(function () {
         });
 
         describe('when the menu icon is clicked', function () {
+            // before each expectation the menu icon will be clicked and the toggle class on the app.js will run
             beforeEach(function () {
                 $('.menu-icon-link').click();
             })
-
+            
+            // since the body has the class 'menu-hidden' by default, when the menu icon is clicked it should remove it.
             it('it should make the menu visibile', function () {
                 expect(body.hasClass('menu-hidden')).not.toBe(true);
             });
-
-            it('if the menu is visible it should hide it', function () {
+            
+            //the second time the beforeEach runs, the click event will add the "menu-hidden" class to the body and hide the menu.
+            it('if the menu is visible, it should hide it', function () {
                 expect(body.hasClass('menu-hidden')).toBe(true);
             });
         })
     });
 
-    describe('Initail Entries', function(){
-        beforeEach(function(done){
+    describe('Initial Entries', function () {
+        // using the setTimeout function to give time to the loadFeed function to finish
+        beforeEach(function (done) {
             loadFeed(0);
-            setTimeout(function() {
-              done()  
-            }, 2000)       
+            setTimeout(function () {
+                done()
+            }, 1000)
         });
-        
-        it ('after the loadFeed is completed, it should have at least a single entry', function(done){
+
+        it('After the loadFeed is completed, it should have at least a single entry', function (done) {
             var entries = $('.feed article.entry');
             expect(entries.length).toBeGreaterThan(0);
             done()
         })
-        
+
     });
 
+    describe('New Feed Selection', function () {
+        var newContent,
+            oldContent;
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        beforeEach(function (done) {
+            // get the content from the entry container before calling the loadFeed function with the click event.
+            oldContent = $('.feed article.entry').html();
+            $('.feed-list a').click();
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-      */
+            // using the setTimeout function to give time to the loadFeed function to finish
+            setTimeout(function () {
+                done();
+            }, 1000)
+
+        });
+
+        it('the content updates when the loadFeed completes', function (done) {
+            // get the content from the entry container after calling the loadFeed function with the click event.
+            newContent = $('.feed article.entry').html();
+            
+            // compares the content before and after the loadFeed function is called.
+            expect(newContent).not.toEqual(oldContent);
+            done();
+        });
+    })
 });
